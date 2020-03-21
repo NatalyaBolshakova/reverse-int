d44a030dfee5e31dc70a2fa50991fdf02a781e2e
NatalyaBolshakova <61007035+NatalyaBolshakova@users.noreply.github.com>
2020-03-21 14:47:34 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    const str = n +'';
+    let pos = str.length - 1;
+    let strN = '';
+    for (pos; pos >= 0; pos--){
+        strN += str[pos];        
+    }
+    return +strN;
 }
